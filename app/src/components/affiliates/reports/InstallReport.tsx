4354731fe33a2d47027aad6997f6a3a3ca76f6b1
ishubin0309 <ivan.shubin0309@gmail.com>
2023-03-17 00:01:56 +0300

    install report draft done

--- app/src/components/affiliates/reports/InstallReport.tsx
+++ app/src/components/affiliates/reports/InstallReport.tsx
@@ -10,172 +10,161 @@ import { DateRangeSelect, useDateRange } from "../../common/DateRangeSelect";
 import { Loading } from "../../common/Loading";
 
 export const InstallReport = () => {
-	const router = useRouter();
-	const { merchant_id } = router.query;
-	const { from, to } = useDateRange();
-	const [traderID, setTraderID] = useState<string>("");
+  const router = useRouter();
+  const { merchant_id, country } = router.query;
+  const { from, to } = useDateRange();
+  const [traderID, setTraderID] = useState<string>("");
 
-	const { data, isLoading } = api.affiliates.getInstallReport.useQuery({
-		from,
-		to,
-		merchant_id: String(merchant_id),
-	});
-	const { data: merchants } = api.affiliates.getAllMerchants.useQuery();
-	const columnHelper = createColumnHelper<InstallReportType>();
+  const { data, isLoading } = api.affiliates.getInstallReport.useQuery({
+    from,
+    to,
+    country: country ? String(country) : undefined,
+    trader_id: traderID ? Number(traderID) : undefined,
+  });
+  const { data: merchants } = api.affiliates.getAllMerchants.useQuery();
+  const { data: countries } = api.affiliates.getLongCountries.useQuery({});
+  const columnHelper = createColumnHelper<InstallReportType>();
 
-	console.log("Install render", {
-		data,
-		merchants,
-		isLoading,
-		from,
-		to,
-		merchant_id,
-	});
+  console.log("Install render", {
+    data,
+    merchants,
+    isLoading,
+    from,
+    to,
+    merchant_id,
+  });
 
-	if (isLoading) {
-		return <Loading />;
-	}
+  if (isLoading) {
+    return <Loading />;
+  }
 
-	const divCol = (
-		val: number | null | undefined,
-		div: number | null | undefined
-	) => {
-		return val ? (
-			<span>{((val / (div || 1)) * 100).toFixed(2)}%</span>
-		) : (
-			<span></span>
-		);
-	};
+  const divCol = (
+    val: number | null | undefined,
+    div: number | null | undefined
+  ) => {
+    return val ? (
+      <span>{((val / (div || 1)) * 100).toFixed(2)}%</span>
+    ) : (
+      <span></span>
+    );
+  };
 
-	const columns = [
-		columnHelper.accessor("type", {
-			cell: (info) => info.getValue(),
-			header: "Event Type",
-		}),
-		columnHelper.accessor("rdate", {
-			cell: (info) => info.getValue(),
-			header: "Event Date",
-		}),
-		columnHelper.accessor("trader_id", {
-			cell: (info) => info.getValue(),
-			header: "Trader ID",
-		}),
-		columnHelper.accessor("trader_alias", {
-			cell: (info) => info.getValue(),
-			header: "Trader Alias",
-		}),
-		// columnHelper.accessor("", {
-		// 	cell: (info) => info.getValue(),
-		// 	header: "Trader Status",
-		// }),
-		columnHelper.accessor("country", {
-			cell: (info) => info.getValue(),
-			header: "Country",
-		}),
-		columnHelper.accessor("affiliate_id", {
-			cell: (info) => info.getValue(),
-			header: "Affiliate ID",
-		}),
-		// columnHelper.accessor("", {
-		// 	cell: (info) => info.getValue(),
-		// 	header: "Affiliate Username",
-		// }),
-		columnHelper.accessor("merchant_id", {
-			cell: (info) => info.getValue(),
-			header: "Merchant ID",
-		}),
-		// columnHelper.accessor("", {
-		// 	cell: (info) => info.getValue(),
-		// 	header: "Merchant Name",
-		// }),
-		// columnHelper.accessor("", {
-		// 	cell: (info) => info.getValue(),
-		// 	header: "Creative ID",
-		// }),
-		// columnHelper.accessor("", {
-		// 	cell: (info) => info.getValue(),
-		// 	header: "Creative Name",
-		// }),
-	];
+  console.log("countries ----->", countries);
 
-	const merchant_options = merchants?.map((merchant) => {
-		return {
-			id: merchant.id,
-			title: merchant?.name,
-		};
-	});
+  const columns = [
+    columnHelper.accessor("type", {
+      cell: (info) => info.getValue() as string,
+      header: "Event Type",
+    }),
+    columnHelper.accessor("rdate", {
+      cell: (info) => info.getValue() as Date,
+      header: "Event Date",
+    }),
+    columnHelper.accessor("trader_id", {
+      cell: (info) => info.getValue() as number,
+      header: "Trader ID",
+    }),
+    columnHelper.accessor("trader_alias", {
+      cell: (info) => info.getValue() as string,
+      header: "Trader Alias",
+    }),
+    columnHelper.accessor("type", {
+      cell: (info) => info.getValue() as string,
+      header: "Trader Status",
+    }),
+    columnHelper.accessor("country", {
+      cell: (info) => info.getValue() as string,
+      header: "Country",
+    }),
+    columnHelper.accessor("affiliate_id", {
+      cell: (info) => info.getValue() as number,
+      header: "Affiliate ID",
+    }),
+    columnHelper.accessor("username", {
+      cell: (info) => info.getValue() as string,
+      header: "Affiliate Username",
+    }),
+    columnHelper.accessor("merchant_id", {
+      cell: (info) => info.getValue() as number,
+      header: "Merchant ID",
+    }),
+    columnHelper.accessor("name", {
+      cell: (info) => info.getValue() as string,
+      header: "Merchant Name",
+    }),
+    columnHelper.accessor("id", {
+      cell: (info) => info.getValue() as number,
+      header: "Creative ID",
+    }),
+    columnHelper.accessor("title", {
+      cell: (info) => info.getValue() as string,
+      header: "Creative Name",
+    }),
+  ];
 
-	let totalAmount = 0;
-	let totalCommission = 0;
-	const totalData = [];
-	data?.forEach((item) => {
-		totalAmount += item.Amount;
-		totalCommission += item.Commission;
-	});
-	totalData.push({
-		totalAmount: totalAmount.toFixed(2),
-		totalCommission: totalCommission.toFixed(2),
-	});
+  const merchant_options = merchants?.map((merchant) => {
+    return {
+      id: merchant.id,
+      title: merchant?.name,
+    };
+  });
 
-	console.log("data", totalData);
+  const country_options = countries?.map((country: any) => {
+    return {
+      id: country.id,
+      title: country.title,
+    };
+  });
 
-	const commissionOption = [
-		{
-			id: "CPA",
-			title: "CPA / TierCPA / DCPA",
-		},
-	];
-
-	return (
-		<>
-			<Grid
-				templateColumns="repeat(4, 1fr)"
-				gap={6}
-				alignContent={"center"}
-				width="90%"
-				alignItems={"center"}
-				alignSelf="center"
-			>
-				<GridItem>
-					<DateRangeSelect />
-				</GridItem>
-				<GridItem>
-					<QuerySelect
-						label="Merchant"
-						choices={merchant_options}
-						varName="merchant_id"
-					/>
-				</GridItem>
-				<GridItem>
-					<FormLabel>Trader ID</FormLabel>
-					<Input
-						value={traderID}
-						onChange={(event) => setTraderID(event.target.value)}
-					/>
-				</GridItem>
-
-				<GridItem>
-					<QuerySelect
-						label="Commission"
-						choices={commissionOption}
-						varName="commission"
-					/>
-				</GridItem>
-			</Grid>
-			<h2>Install Report</h2>
-			<Grid
-				alignContent={"center"}
-				alignItems={"center"}
-				width="100%"
-				alignSelf="center"
-				overflow={"scroll"}
-			>
-				<DataTable
-					data={Object.values(data || {})}
-					columns={columns}
-					footerData={totalData}
-				/>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Grid
+        templateColumns="repeat(4, 1fr)"
+        gap={6}
+        alignContent={"center"}
+        width="90%"
+        alignItems={"center"}
+        alignSelf="center"
+      >
+        <GridItem>
+          <DateRangeSelect />
+        </GridItem>
+        <GridItem>
+          <QuerySelect
+            label="Merchant"
+            choices={merchant_options}
+            varName="merchant_id"
+          />
+        </GridItem>
+        <GridItem>
+          <QuerySelect
+            label="Country"
+            choices={country_options}
+            varName="country"
+          />
+        </GridItem>
+        <GridItem>
+          <FormLabel>Trader ID</FormLabel>
+          <Input
+            value={traderID}
+            onChange={(event) => setTraderID(event.target.value)}
+          />
+        </GridItem>
+      </Grid>
+      <h2>Install Report</h2>
+      <Grid
+        alignContent={"center"}
+        alignItems={"center"}
+        width="100%"
+        alignSelf="center"
+        overflow={"scroll"}
+      >
+        <DataTable
+          data={Object.values(data || {})}
+          columns={columns}
+          footerData={[]}
+        />
+      </Grid>
+    </>
+  );
 };
--- app/src/pages/affiliates/reports/install-reports.tsx
+++ app/src/pages/affiliates/reports/install-reports.tsx
@@ -4,21 +4,18 @@ import { InstallReport } from "../../../components/affiliates/reports/InstallRep
 import styles from "./../../index.module.css";
 
 const Page: NextPage = () => {
-	return (
-		<>
-			<Head>
-				<title>Install Report</title>
-				<meta
-					name="description"
-					content="Affiliates Creative Materials"
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<main className={styles.main}>
-				<InstallReport />
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Install Report</title>
+        <meta name="description" content="Affiliates Creative Materials" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <InstallReport />
+      </main>
+    </>
+  );
 };
 
 export default Page;
