5b49468c423de2ed4e9b9eb3f50eac75b16d277a
ishubin0309 <ivan.shubin0309@gmail.com>
2023-03-29 21:05:51 +0200

    testing

---
+++ app/src/components/affiliates/dashboard/dashboard.stories.tsx
@@ -0,0 +1,13 @@
+import AuthenticationFooterComponent from "@/components/common/footer/AuthenticationFooter";
+import React, { useState } from "react";
+
+const meta = {
+  component: AuthenticationFooterComponent,
+};
+
+export default meta;
+
+export const AuthenticationFooter = {
+  render: () => <AuthenticationFooterComponent />,
+};
+
