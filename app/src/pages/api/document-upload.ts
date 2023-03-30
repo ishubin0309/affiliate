14bc165dbd79cd66f88c38a28696b1c4cd17e1a6
ishubin0309 <ivan.shubin0309@gmail.com>
2023-03-30 08:34:18 +0300

    test document upload

--- app/src/pages/api/document-upload.ts
+++ app/src/pages/api/document-upload.ts
@@ -75,12 +75,16 @@ export async function handler(req: NextApiRequest, res: NextApiResponse) {
     );
     form.append("shared-secret", env.LEGACY_PHP_ACCESS_TOKEN);
 
-    const apiRes = await axios.post(`${String(env.LEGACY_PHP_API_URL)}`, form, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
+    const apiRes = await axios.post(
+      `${String(env.LEGACY_PHP_API_URL)}/ajax/UploadDocumentsApp.php`,
+      form,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
 
     console.log(`muly:handler fetch:POST answer`, {
       apiRes,
