6cf1af000bb9adf874389556666b628e68083d9f
ishubin0309 <ivan.shubin0309@gmail.com>
2023-04-03 17:16:52 +0200

    dialog close btn fixed

--- app/src/components/ui/dialog.tsx
+++ app/src/components/ui/dialog.tsx
@@ -56,7 +56,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="focus:ring-slate-400 data-[state=open]:bg-slate-100 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900 dark:data-[state=open]:bg-slate-800 absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <DialogPrimitive.Close className="data-[state=open]:bg-slate-100 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900 dark:data-[state=open]:bg-slate-800 absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none">
         {/* <X className="h-4 w-4" />
         <span className="sr-only">Close</span> */}
         <svg
