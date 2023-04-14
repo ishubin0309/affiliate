fd8c4ba052a23379bdda2c6618f2de4ef699a432
ishubin0309 <ivan.shubin0309@gmail.com>
2023-04-14 12:04:39 +0500

    Date function added

--- app/src/server/api/routers/affiliates/dashboard-device-report.ts
+++ app/src/server/api/routers/affiliates/dashboard-device-report.ts
@@ -1,3 +1,4 @@
+import { sub } from "date-fns";
 import { z } from "zod";
 import { publicProcedure } from "../../trpc";
 import { affiliate_id, merchant_id } from "./const";
@@ -9,8 +10,7 @@ export const getDashboardDeviceReport = publicProcedure
     })
   )
   .query(async ({ ctx, input: { lastDays } }) => {
-    const dateQuery = new Date();
-    dateQuery.setDate(dateQuery.getDate() - lastDays ? lastDays : 0);
+    const dateQuery = sub(new Date(), { days: lastDays ? lastDays : 0 });
     const data = await ctx.prisma.merchants_creative_stats.groupBy({
       by: ["merchant_id", "CountryID"],
       where: {
