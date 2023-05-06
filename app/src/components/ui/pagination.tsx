12d9e3eacc8a74395e55b51ebc2bcb7d8fe6473f
ishubin0309 <ivan.shubin0309@gmail.com>
2023-05-06 15:10:41 +0500

    replace custom css class to tailwind classes

--- app/src/components/ui/pagination.tsx
+++ app/src/components/ui/pagination.tsx
@@ -42,7 +42,7 @@ export const Pagination = ({
   };
 
   return (
-    <nav className="flex items-center justify-start space-x-0 paginate-container">
+    <nav className="flex items-center justify-start space-x-0 max-w-full overflow-auto flex-wrap">
       <ReactPaginate
         breakLabel="..."
         nextLabel={<ChevronsRight />}
