85a3eea7d899bf4b68bec76a3d977e927d3b166a
ishubin0309 <ivan.shubin0309@gmail.com>
2023-05-07 09:17:33 +0300

    column select cleanup

--- app/src/components/affiliates/reports/report-control.tsx
+++ app/src/components/affiliates/reports/report-control.tsx
@@ -3,7 +3,10 @@ import type { OnExport } from "@/components/affiliates/reports/utils";
 import { Loading } from "@/components/common/Loading";
 import type { ReportDataTableProps } from "@/components/common/data-table/ReportDataTable";
 import { ReportDataTable } from "@/components/common/data-table/ReportDataTable";
-import ColumnSelect from "@/components/common/data-table/column-select";
+import {
+  ColumnSelect,
+  getColumnsBySetup,
+} from "@/components/common/data-table/column-select";
 import type { usePagination } from "@/components/common/data-table/pagination-hook";
 import { PageHeader } from "@/components/common/page/page-header";
 import { SearchApply } from "@/components/common/search/saerch-apply-button";
@@ -14,6 +17,7 @@ import { useToast } from "@/components/ui/use-toast";
 import { api } from "@/utils/api";
 import { SaveIcon, SettingsIcon } from "lucide-react";
 import React, { useState } from "react";
+import { ColumnSelectButton } from "@/components/common/data-table/column-select-button";
 
 interface Props<Data extends object> extends ReportDataTableProps<Data> {
   reportName: string;
@@ -33,25 +37,10 @@ export const ReportControl = <Data extends object>({
   footerData,
   handleExport,
 }: Props<Data>) => {
-  const { toast } = useToast();
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectColumnsMode, setSelectColumnsMode] = useState<string[] | null>(
-    null
-  );
+  const [selectColumnsMode, setSelectColumnsMode] = useState<{
+    [name: string]: boolean;
+  } | null>(null);
 
-  const handleColumnChange = (fieldName: string, checked: boolean) => {
-    if (selectColumnsMode) {
-      if (checked) {
-        setSelectColumnsMode([...selectColumnsMode, fieldName]);
-      } else {
-        setSelectColumnsMode(
-          selectColumnsMode.filter((item) => item !== fieldName)
-        );
-      }
-    }
-  };
-
-  const apiContext = api.useContext();
   const { data: reportsColumns } = api.affiliates.getReportsColumns.useQuery(
     { level: "affiliate", report: reportName },
     {
@@ -60,33 +49,7 @@ export const ReportControl = <Data extends object>({
     }
   );
 
-  const upsertReportsColumns =
-    api.affiliates.upsertReportsColumns.useMutation();
-
-  const handleSelectMode = async () => {
-    setIsLoading(true);
-    try {
-      if (selectColumnsMode) {
-        const columns = await upsertReportsColumns.mutateAsync({
-          level: "affiliate",
-          report: reportName,
-          fields: selectColumnsMode || [],
-        });
-
-        apiContext.affiliates.getReportsColumns.setData(
-          { level: "affiliate", report: reportName },
-          columns
-        );
-      }
-      setSelectColumnsMode(selectColumnsMode ? null : reportsColumns || []);
-      toast({
-        title: "Saved " + reportName + " Setup",
-        duration: 5000,
-      });
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  console.log(`muly:ReportControl`, { reportsColumns });
 
   return report ? (
     <div className="flex w-full flex-col gap-2">
@@ -104,33 +67,27 @@ export const ReportControl = <Data extends object>({
       >
         <div className="flex flex-row gap-2">
           <ExportButton onExport={handleExport} />
-          <Button
-            variant="primary"
-            size="rec"
-            onClick={handleSelectMode}
-            isLoading={isLoading}
-          >
-            {selectColumnsMode ? (
-              <SaveIcon className="w-4" />
-            ) : (
-              <SettingsIcon />
-            )}
-          </Button>
+          <ColumnSelectButton
+            columns={columns}
+            reportName={reportName}
+            reportsColumns={reportsColumns}
+            selectColumnsMode={selectColumnsMode}
+            setSelectColumnsMode={setSelectColumnsMode}
+          />
         </div>
       </PageHeader>
 
       <ColumnSelect
         columns={columns}
+        reportsColumns={reportsColumns}
         selectColumnsMode={selectColumnsMode}
-        handleCheckboxChange={handleColumnChange}
+        setSelectColumnsMode={setSelectColumnsMode}
       />
 
       {report.pageInfo.totalItems > 0 ? (
         <ReportDataTable
           report={report}
-          columns={columns.filter((item: any) =>
-            reportsColumns?.includes(item.accessorKey)
-          )}
+          columns={getColumnsBySetup(columns, reportsColumns)}
           footerData={footerData}
         />
       ) : (
--- app/src/components/common/data-table/column-select.tsx
+++ app/src/components/common/data-table/column-select.tsx
@@ -1,46 +1,74 @@
 import { Checkbox } from "@/components/ui/checkbox";
-import { ColumnDef } from "@tanstack/react-table";
+import type { ColumnDef } from "@tanstack/react-table";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+export type SelectedColumnList = Record<string, boolean>;
 
 interface Props<Data extends object> {
-  columns: ColumnDef<Data, any>[];
-  selectColumnsMode: string[] | null;
-  handleCheckboxChange: (fieldName: string, checked: boolean) => void;
+  reportsColumns?: string[];
+  columns: ColumnDef<any, any>[];
+  selectColumnsMode: SelectedColumnList | null;
+  setSelectColumnsMode: (selectedStatus: SelectedColumnList | null) => void;
 }
 
-const ColumnSelect = <Data extends object>({
+export const ColumnSelect = <Data extends object>({
+  reportsColumns,
   columns,
+  setSelectColumnsMode,
   selectColumnsMode,
-  handleCheckboxChange,
 }: Props<Data>) => {
+  const handleColumnChange = (fieldName: string, checked: boolean) => {
+    if (selectColumnsMode) {
+      setSelectColumnsMode({ ...selectColumnsMode, [fieldName]: checked });
+    }
+  };
+
   return (
     <div
-      className={`mt-4 overflow-hidden transition-all duration-500	 ${
-        selectColumnsMode ? "h-52 md:h-44 lg:h-36 xl:h-28" : "h-0"
-      }`}
+      className={cn(
+        "mt-4 overflow-hidden transition-all duration-500",
+        { "h-52 md:h-44 lg:h-36 xl:h-28": !!selectColumnsMode },
+        { "h-0": !selectColumnsMode }
+      )}
     >
       <div className="grid grid-cols-2 gap-2 rounded-lg bg-white p-4 shadow-md md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-9">
-        {columns.map((item: any) => (
-          <div className="flex items-center space-x-2" key={item.accessorKey}>
-            <Checkbox
-              className="mr-2 h-[18px] w-[18px] whitespace-nowrap"
-              id={item.header}
-              name={item.accessorKey}
-              checked={selectColumnsMode?.includes(item.accessorKey)}
-              onCheckedChange={(checked: boolean) => {
-                handleCheckboxChange(item.accessorKey, checked);
-              }}
-            />
-            <label
-              htmlFor={item.header}
-              className="cursor-pointer text-sm font-medium leading-none"
-            >
-              {item.header}
-            </label>
-          </div>
-        ))}
+        {columns.map((item) => {
+          const name = String(item.header);
+          return (
+            <div className="flex items-center space-x-2" key={name}>
+              <Checkbox
+                className="mr-2 h-[18px] w-[18px] whitespace-nowrap"
+                id={name}
+                name={name}
+                checked={!!selectColumnsMode && !!selectColumnsMode[name]}
+                onCheckedChange={(checked: boolean) => {
+                  handleColumnChange(name, checked);
+                }}
+              />
+              <label
+                htmlFor={name}
+                className="cursor-pointer text-sm font-medium leading-none"
+              >
+                {name}
+              </label>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
 
-export default ColumnSelect;
+export const getColumnsBySetup = <Data,>(
+  columns: ColumnDef<Data, any>[],
+  reportsColumns?: string[]
+): ColumnDef<Data, any>[] => {
+  if (!columns || !columns.length || !columns[0]) return [];
+
+  const filteredColumns = columns.filter(
+    (item: any) => !reportsColumns?.includes(String(item.header))
+  );
+
+  return filteredColumns.length ? filteredColumns : [columns[0]];
+};
