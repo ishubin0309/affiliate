b43c22577e399bf82ea8f3813a516f0cbd25b6de
ishubin0309 <ivan.shubin0309@gmail.com>
2023-05-08 15:24:10 +0500

    replaced svg from public to react component

--- app/src/components/common/Loading.tsx
+++ app/src/components/common/Loading.tsx
@@ -1,7 +1,8 @@
+import { LoadingIcon } from "@/components/icons";
 export const Loading = () => {
   return (
     <div className="absolute left-0 right-0 top-0 flex h-full items-center justify-center md:ml-72">
-      <img src={"/img/bars-scale-fade.svg"} alt="My SVG file" />
+      <LoadingIcon />
     </div>
   );
 };
