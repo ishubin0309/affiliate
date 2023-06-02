b3e14765c6a1fcd38a6d8a6cf732aec267d96427
ishubin0309 <ivan.shubin0309@gmail.com>
2023-06-02 17:54:30 +0500

    added new icons and thier styling

--- app/src/components/common/forms/IconMenuRadioGroup.tsx
+++ app/src/components/common/forms/IconMenuRadioGroup.tsx
@@ -11,15 +11,14 @@ type IconMenuRadioGroupProps = {
 };
 
 const toggleGroupItemClasses =
-  "rounded-sm hover:bg-violet3 color-mauve11 data-[state=on]:bg-violet6 data-[state=on]:text-violet12 flex h-[35px] w-[35px] items-center justify-center bg-white text-base leading-4 first:rounded-l last:rounded-r focus:z-10 focus:shadow-[0_0_0_2px] focus:shadow-black";
+  "rounded-sm flex h-[35px] w-[35px] items-center justify-center text-[#888888] leading-4 first:rounded-l last:rounded-r focus:z-10 focus:shadow-[0_0_0_0px] focus:shadow-black";
 
-const activeToggleGroupItemClasses = "bg-slate-300 text-white";
+const activeToggleGroupItemClasses = "bg-[#eaeaea] text-[#000000]";
 
 export const IconMenuRadioGroup = ({
   gridView,
   onGridViewChange,
 }: IconMenuRadioGroupProps) => {
-
   return (
     <ToggleGroup.Root
       className="bg-mauve6 inline-flex space-x-px rounded-sm bg-white p-1"
@@ -29,18 +28,18 @@ export const IconMenuRadioGroup = ({
       aria-label="Text alignment"
     >
       <ToggleGroup.Item
-        className={`${toggleGroupItemClasses} ${
+        className={`${
           gridView ? activeToggleGroupItemClasses : ""
-        }`}
+        } ${toggleGroupItemClasses} `}
         value="left"
         aria-label="Left aligned"
       >
         <GridToggleIcon />
       </ToggleGroup.Item>
       <ToggleGroup.Item
-        className={`${toggleGroupItemClasses} ${
+        className={`${
           !gridView ? activeToggleGroupItemClasses : ""
-        }`}
+        } ${toggleGroupItemClasses} `}
         value="right"
         aria-label="Right aligned"
       >
